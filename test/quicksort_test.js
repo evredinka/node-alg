728dbdb82d9f7f976cd98ba89c682e5409d56ba6
evgeniia.prozorova <evgeniia.prozorova@auto1.com>
2019-04-14 17:32:03 +0200

    quicksort

---
+++ test/quicksort_test.js
@@ -0,0 +1,22 @@
+let sort = require('../lib/quicksort').sort;
+const expect = require("chai").expect;
+
+describe('Quick Sort', function () {
+
+    it('should sort elements in the list', function () {
+        expect(sort([1, 5, 3, 7])).to.eql([1, 3, 5, 7]);
+    });
+
+    it('should sort elements in the list', function () {
+        expect(sort([5, 4, 2, 3, 1])).to.eql([1, 2, 3, 4, 5]);
+    });
+
+    it('should sort elements in empty list', function () {
+        expect(sort([])).to.eql([]);
+    });
+
+    it('should sort elements in singleton list', function () {
+        expect(sort([3])).to.eql([3]);
+    });
+
+});
